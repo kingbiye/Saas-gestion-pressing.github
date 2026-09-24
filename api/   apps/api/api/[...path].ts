7d45a16cf887c6b7d9d61d0bfa82e0b7eac2a901
kingbiye <kingbiye3@gmail.com>
2026-09-24 20:43:17 +0100

    Delete api/   apps/api/api/[...path].ts

--- api/   apps/api/api/[...path].ts
+++
@@ -1,9 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
-import { handleRequest } from "../../src/main";
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return handleRequest(req, res).catch(() => {
-    res.writeHead(400, { "content-type": "application/json" });
-    res.end(JSON.stringify({ error: "INVALID_REQUEST" }));
-  });
-}
